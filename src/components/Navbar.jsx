cbe21ce289358419fdc8a5ec70d1df31aa99b1ef
Coops023 <bjorkc27@gmail.com>
2021-12-08 16:33:33 +0100

    contact page complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,63 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-dark ">
+      <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img
+              src="headshot.jpg"
+              alt=""
+              width="70"
+              className="d-inline-block align-text-top"
+            />
+            <h2 className="name-logo">Cooper Bjorkelund</h2>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarTogglerDemo02"
+          >
+            <ul className="nav justify-content-end">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/projects"
+                >
+                  Projects
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/contact"
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* <nav className="navbar navbar-dark ">
+        <div className="container-fluid">
+      
           <NavLink className="navbar-brand" to="/">
             <img
               src="headshot.jpg"
@@ -42,7 +97,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,38 +1,120 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
+import "./Contact.css";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Contact = () => {
   const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_cvk3fcr",
-        "template_ltghbve",
-        form.current,
-        "user_AcCQo8isxokY8Pb3autGq"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (name === "" || email === "" || message === "") {
+      showModal();
+    } else {
+      emailjs
+        .sendForm(
+          "service_cvk3fcr",
+          "template_ltghbve",
+          form.current,
+          "user_AcCQo8isxokY8Pb3autGq"
+        )
+        .then(
+          (result) => {
+            console.log(result);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="container">
+      <div class="row">
+        <div class="content col-12">
+          <h1 className="heading">Want to get in-touch?</h1>
+          <p className="paragraph">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+            molestiae excepturi incidunt, laborum provident quasi aliquam
+            repellat perspiciatis, totam iusto dicta! Saepe suscipit ipsum
+            explicabo vel aut possimus quod incidunt.
+          </p>
+          <form className="contact-form" ref={form} onSubmit={sendEmail}>
+            <label className="form-label">Name</label>
+            <input
+              className="form-input"
+              type="text"
+              name="name"
+              placeholder="Your name"
+              value={name}
+              onChange={handleNameChange}
+            />
+            <label className="form-label">Email</label>
+            <input
+              className="form-input"
+              type="email"
+              name="email"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <label className="form-label">Message</label>
+            <textarea
+              className="message-input"
+              name="message"
+              placeholder="leave a message!"
+              value={message}
+              onChange={handleMessageChange}
+            />
+            <button
+              className="form-button"
+              type="submit"
+              value="Send"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              Send Message
+            </button>
+            <Modal show={isOpen} onHide={hideModal}>
+              <Modal.Header>
+                <Modal.Title>Error!</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Error: fill all fields</Modal.Body>
+              <Modal.Footer>
+                <button onClick={hideModal}>Hide</button>
+              </Modal.Footer>
+            </Modal>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
