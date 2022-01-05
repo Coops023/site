8def99b44f6338d195968bce03c149eeb68352d2
Coops023 <bjorkc27@gmail.com>
2022-01-05 20:31:47 +0100

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,30 +4,25 @@ import "./About.css";
 export default function About() {
   return (
     <div>
-      <div class="container">
-        <div class="row">
-          <div class="about-content col-12">
-            <div className="about-text-wrap">
-              <h1 className="about-heading">Hey, Im Cooper!</h1>
+      <div class="about-content">
+        <div className="about-text-wrap">
+          <h1 className="about-heading">Hey, Im Cooper</h1>
 
-              <p className="about-paragraph">
-                I’m an amazing full stack web developer, bla bla bla. Saying in
-                the first lines that you are a recently graduate is not that
-                sexy :) You gotta sell yourself better. Again not what you’ve
-                learned, technologies that you’re using in your workflow.
-                Confidence this is what I want to get by reading this and
-                experience.
-                <a className="contact-span" href="/contact">
-                  {" "}
-                  contact with me
-                </a>{" "}
-                i would love to hear from you.
-              </p>
-            </div>
-
-            <img src="./me-img.png" alt="an image of a man" />
-          </div>
+          <p className="about-paragraph">
+            I’m an amazing full stack web developer, bla bla bla. Saying in the
+            first lines that you are a recently graduate is not that sexy :) You
+            gotta sell yourself better. Again not what you’ve learned,
+            technologies that you’re using in your workflow. Confidence this is
+            what I want to get by reading this and experience.
+            <a className="contact-span" href="/contact">
+              {" "}
+              contact with me
+            </a>{" "}
+            i would love to hear from you.
+          </p>
         </div>
+
+        <img src="./me-img.png" alt="an image of a man" />
       </div>
     </div>
   );
